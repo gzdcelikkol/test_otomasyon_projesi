3c1a765056398689e82bb4d38c66fd544f3cf3d3
gozde.celikkol <gozdecelikkol@hotmail.com>
2026-09-01 12:01:56 +0300

    ElementHelper dosyası

---
+++ src/test/java/helpers/ElementHelper.java
@@ -0,0 +1,32 @@
+package helpers;
+
+import driverManager.Driver;
+import locators.LocatorManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+public class ElementHelper {
+
+    public static WebElement findElement(String pageName, String elementName){
+        By locator = LocatorManager.getLocator(pageName, elementName);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+  // elementi bulup görünür olması için bekler
+    }
+
+    // bulunan elemente tıklayabilmek için
+    public static void click(String pageName, String elementName){
+        WebElement element = findElement(pageName, elementName);
+        element.click();
+    }
+
+    // elemente bir veri girişi yapmak için örn. login page'de username, password
+    public static void sendKeys(String pageName, String elementName, String text){
+        WebElement element = findElement(pageName, elementName);
+        element.clear();
+        element.sendKeys(text);
+    }
+}
